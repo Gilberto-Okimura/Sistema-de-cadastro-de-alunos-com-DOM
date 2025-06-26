e58b651d25b1a02aca8a285d472ee125d99492d2
gilbas <jokalima16@gmail.com>
2025-06-26 12:11:29 -0500

    Refactor form handling and list display; add CSS file

--- src/js.js
+++ src/js.js
@@ -1,47 +1,48 @@
 
 
-let name_f = document.getElementById('nome').value
-let year_f = document.getElementById('idade').value
-let course_f = document.getElementById('curso')
+
 let form = document.querySelector('form')
+let alunos = [];
 
 
 
 form.onsubmit = function(event){ //Quando for enviado
   event.preventDefault() // quando vier default (nulo)
 
+   
+
   const formdata = new FormData(form) //usa uma função do js que pega toda informação do form. 
   const datatosave = { // aqui e para pegar cada elemento na query.
     nome: formdata.get('nome') ,
     idade: formdata.get('idade'),
     curso: formdata.get('curso')
-  }
+         }   
+         alunos.push(datatosave)
+         list()
+        
+         console.log(alunos)
+}
 
 
-  let alunos = [datatosave] //armazena em um array
+function list() {
+   const ulpai = document.getElementById("lista-alunos")
+   ulpai.innerHTML = '' // Limpa a lista antes de recriar
 
-  console.log(alunos);
+   alunos.forEach((aluno, index) => {
+      const criarLi = document.createElement('li')
+      ulpai.appendChild(criarLi)
+      const criarSelect = document.createElement('input')
+      criarSelect.type = 'checkbox'
+      criarSelect.dataset.index = index
 
- function list(){
-    let criarLi = document.createElement('li')
-    let ulpai = document.getElementById("lista-alunos")
-
-    for(let i = 0; i < alunos.length; i++){
-       let criarbotao_ofdel = document.createElement('button')
-          let ulcriar = ulpai.appendChild(criarLi )
-          ulcriar.innerText += `Aluno: nome:${datatosave.nome} idade:${datatosave.idade} curso: ${datatosave.curso}`
-
-
-    }
-   
-
-   
-
- }
-   list()
+      ulpai.addEventListener('',(e)=>{
+      
+      })
     
+      
 
-  
+      criarLi.textContent += `Aluno ${index + 1}: Nome: ${aluno.nome}, Idade: ${aluno.idade}, Curso: ${aluno.curso}`
+   })
 }
 
 
